7f5c6f9c3f402ed83d57a54e0cce1b6483a9c31b
hadove02 <hadove02@naver.com>
2024-06-12 17:40:11 +0900

    캠핑 등록 수정

--- models/Camp.js
+++ models/Camp.js
@@ -1,28 +1,17 @@
 const conn = require('../db');
 
 const Camp = {
+
     // 캠핑장 등록
     register: (req, res) => {
+        const {userNo, campingType, campingName, address, contact, description, checkInTime, checkOutTime, mainImage, mannerTimeStart, mannerTimeEnd, facilities, activities, environment, localType} = req.body;
         conn.query(
             `
-                INSERT INTO campground (userNo, facilitiesInfoNo, mannerStartTime, mannerEndTime, campGroundImages, name,
-                                        location, type, callNum, campingInfo, enterTime, leaveTime)
-                SELECT ?,
-                       MAX(facilitiesInfoNo),
-                       ?,
-                       ?,
-                       ?,
-                       ?,
-                       ?,
-                       ?,
-                       ?,
-                       ?,
-                       ?,
-                       ?
-                FROM facilitiesinfo;
-            `,
+                INSERT INTO campground (userNo, type, name, location, callNum, campingInfo, enterTime, leaveTime, campGroundImages, mannerStartTime, mannerEndTime, facilities, activities, environment, localType)
+                    VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)
+                `,
             [
-                req.body.userNo, req.body.mannerStartTime, req.body.mannerEndTime, req.body.campGroundImages, req.body.name, req.body.location, req.body.type, req.body.callNum, req.body.campingInfo, req.body.enterTime, req.body.leaveTime
+                userNo, campingType, campingName, address, contact, description, checkInTime, checkOutTime, mainImage, mannerTimeStart, mannerTimeEnd, facilities, activities, environment, localType
             ], (err, result) => {
                 if (err) {
                     console.log(err)
