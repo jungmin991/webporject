e9a799df6057d842091bc32444b737f3005dec3f
hadove02 <hadove02@naver.com>
2024-06-15 21:14:25 +0900

    캠핑장 등록 완성

--- src/Owner/CampGround/RegistCampGround3.js
+++ src/Owner/CampGround/RegistCampGround3.js
@@ -1,11 +1,15 @@
 import axios from "axios";
 import {requestURL} from "../../config/config";
+import {useNavigate} from "react-router-dom";
 
-export default function RegistCampGround3(campGroundInfo, setCampGroundInfo) {
+export default function RegistCampGround3({campGroundInfo, setCampGroundInfo}) {
+    const navigate = useNavigate();
+
     async function register() {
-        console.log(campGroundInfo.campGroundInfo);
-        await axios.post(requestURL+"/campground/register",campGroundInfo.campGroundInfo)
+        navigate("/host/" + campGroundInfo.userNo)
+        await axios.post(requestURL + "/campground/register", campGroundInfo)
     }
+
     return <div>
         <label><input type="image"/>대표 이미지<br/></label>
         <button onClick={register}/>
