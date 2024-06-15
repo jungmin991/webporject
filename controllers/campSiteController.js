6833dc2df087478b5fa4d26278013ef30028d6ac
hadove02 <hadove02@naver.com>
2024-06-15 22:05:50 +0900

    캠프사이트 등록

--- controllers/campSiteController.js
+++ controllers/campSiteController.js
@@ -3,3 +3,7 @@ const site = require('../models/CampGroundSite')
 exports.register = (req, res) => {
     site.register(req, res);
 }
+
+exports.siteList = (req, res) => {
+    site.siteList(req, res);
+}
